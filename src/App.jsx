269596a1422f25b74f27936ef3b55da1688c508e
danielemurgioni <dashomew@gmail.com>
2025-05-20 12:42:12 +0200

    [UPD] setting input property "value" with hook "useState" and change the value with event-handler "onChange"

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react"
+
 function App() {
+
+  const [search, setSearch] = useState("")
 
   return (
     <>
@@ -6,16 +10,24 @@ function App() {
         <h1>React BoolFlix</h1>
       </header>
       <main>
+
         <div className="container search-bar">
-          <input type="text" />
+          {/* input to search */}
+          <input type="text"
+            value={search}
+            onChange={(e) => (setSearch(e.target.value), console.log(e.target.value))} />
+          {/* button activate search */}
           <button>Search</button>
         </div>
+
+        {/* card */}
         <div className="container film-data mt-40">
           <h2>titolo film</h2>
           <h2>titolo originale del film</h2>
           <h2>Lingua</h2>
           <h2>Voto</h2>
         </div>
+
       </main>
     </>
   )
