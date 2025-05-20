4b68610259e01b7bb444f5b16aa16737226330c8
danielemurgioni <dashomew@gmail.com>
2025-05-20 16:24:35 +0200

    [UPD/FIX] render arrMovies; fix button and "arrMovies useState" and optimazed endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -14,19 +14,18 @@ function App() {
   //hook useState --> aggiorna la valuta dell'endpoint tramite il button search onClick
   const [queryValue, setQueryValue] = useState(inpValue)
 
-  //assegno a una variabile l'endpoint che si aggiorna in base al valore dell'input tramite il button search
-  const endpointMovies = `https://api.themoviedb.org/3/search/movie?api_key=f8ab9584bebbbf818e62d87d46593a6b&query=${queryValue}`
-  console.log(endpointMovies)
 
   //hook useState --> contiene l'array che viene aggiornato tramite useEffect
-  const [arrMovies, setArrMovies] = useState()
+  const [arrMovies, setArrMovies] = useState([])
 
   //hook useEffect --> esegue il codice ogni volta che l'endpoint viene aggiornato
   useEffect(() => {
+    //assegno a una variabile l'endpoint che si aggiorna in base al valore dell'input tramite il button search
+    const endpointMovies = `https://api.themoviedb.org/3/search/movie?api_key=f8ab9584bebbbf818e62d87d46593a6b&query=${queryValue}`
+    console.log(endpointMovies)
     //chiamo l'endpoint tramite axios e salvo i dati nella variabile di stato arrMovies tramite useState
     axios.get(endpointMovies).then((res) => setArrMovies(res.data.results))
-  }, [endpointMovies])
-
+  }, [queryValue])
   console.log(arrMovies)
 
   return (
@@ -42,18 +41,22 @@ function App() {
             value={inpValue}
             onChange={(e) => (setInpValue(e.target.value))} />
           {/* button activate search */}
-          <button className="btn-search" onClick={setQueryValue}>Search</button>
+          <button className="btn-search" onClick={() => setQueryValue(inpValue)}>Search</button>
         </div>
 
         {/* card */}
-        <div className="container film-data mt-40">
-          <h2>titolo film</h2>
-          <h2>titolo originale del film</h2>
-          <h2>Lingua</h2>
-          <h2>Voto</h2>
+        <div className="container movie-data mt-40">
+          {arrMovies.map((movie) => (
+            <div key={movie.id} className="movie-info m-20" >
+              <p><strong>titolo film -</strong> {movie.title}</p>
+              <p><strong>titolo originale del film -</strong> {movie.original_title}</p>
+              <p><strong>Lingua -</strong> {movie.original_language}</p>
+              <p><strong>Voto -</strong> {movie.vote_average}</p>
+            </div>
+          ))}
         </div>
 
-      </main>
+      </main >
     </>
   )
 }
